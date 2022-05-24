260b3cff1d62da52228c90ab6926e0884c4b5efe
Arthur <arthur_2335@hotmail.com>
2022-05-24 15:53:45 -0400

    added and fixed 3 buttons

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,17 +1,21 @@
 import { View, Text, Button, StyleSheet } from "react-native";
+import { useState } from "react";
 
 export default function Counter() {
+    const [count, setCount] = useState(0)
     return(
         <View style={styles.counterContainer}>
-            <Text style={styles.counterText}>You Clicked the Button 0 times.</Text>
-            <Button title='Click Me' />
+            <Text style={styles.counterText}>You Clicked the Button {count} times.</Text>
+            <Button title='Addition' onPress={() => setCount(count + 1)} />
+            <Button title='Reset' onPress={() => setCount(0)} />
+            <Button title='Subtraction' onPress={() => setCount(count - 1)} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     counterContainer: {
-        width: '80vw',
+        width: '80%',
         margin: 14,
         padding: 14,
         backgroundColor: 'beige',
