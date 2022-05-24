4ab6bba1f1f53fa52794a6d3c99804e48a0ecd24
Arthur <arthur_2335@hotmail.com>
2022-05-24 15:18:55 -0400

    started app and changed font color

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>Open up App.js to start working on your app!</Text>
+      <Button title='Press Me Please' color='#841584'/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  heading: {
+    fontSize: 38,
+    color: '#fff'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffaa00',
     alignItems: 'center',
     justifyContent: 'center',
   },
